4e2bbfe16be3a1cffbe07381e9fa9a6f7f2213d8
Chase <cmeadows_17@yahoo.com>
2026-03-17 16:37:27 -0400

    deleting useless code

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Document from "./components/Document";
 import "./App.css";
 
 export default function PianoApp() {
-  const firstNote = MidiNumbers.fromNote("A0");
-  const lastNote = MidiNumbers.fromNote("C8");
-  const notesInRange = Array.from(
-    { length: lastNote - firstNote + 1 }, // Create an array with the correct length
-    (_, index) => firstNote + index, // Fill the array with the notes
-  );
-
   useEffect(() => {
+    const firstNote = MidiNumbers.fromNote("A0");
+    const lastNote = MidiNumbers.fromNote("C8");
+    const notesInRange = Array.from(
+      { length: lastNote - firstNote + 1 }, // Create an array with the correct length
+      (_, index) => firstNote + index, // Fill the array with the notes
+    );
     console.log(notesInRange);
   }, []);
 
